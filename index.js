95d160bf898f5250be5b7fa2b2a6f172f2e38e4a
a.s.senina <a.s.senina@tinkoff.ru>
2024-01-19 11:07:43 +0300

    Добавила losash и поправила gitignore

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("hello, hexlet!")
+import _ from "lodash";
+
+// Пересечение массивов
+console.log(_.intersection([2, 1], [2, 3]))
+
+// capitalize делает первую букву заглавной
+console.log(_.capitalize("hello"))
